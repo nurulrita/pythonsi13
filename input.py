297b9952302de734c40edce4f10209150bf41c01
nuthanu <nuthanuu@gmail.com>
2015-09-01 15:17:24 +0700

    first commit

---
+++ input.py
@@ -0,0 +1,4 @@
+print "selamat datang :)"
+nama = raw_input("masukkan nama anda:")
+umur = input("masukkan umur anda:")
+print "Haiiii {0}, umur anda {1} ya".format(nama,umur)
---
+++ segitiga.py
@@ -0,0 +1,4 @@
+alas = input("masukkan alas segitiga :")
+tinggi = input("masukkan tinggi segitiga:")
+
+print "luas segitiga adalah {0}".format(alas*tinggi/2)
